5826d1f16d026ba1a93b555716660927ba8f4758
raderro <95587591+raderro@users.noreply.github.com>
2026-05-11 13:30:56 +0200

    update

--- or-data-check/Services/ExcelServices.cs
+++ or-data-check/Services/ExcelServices.cs
@@ -18,35 +18,45 @@ namespace or_data_check
                 throw new FileNotFoundException($"Excel file not found: {filePath}");
             }
 
-            DataTable dt = new DataTable(); //tu jest przykład miejsca, gdzie można użyć przy deklaracji słowa kluczowego var - bo widać jaki jest typ
+            var dt = new DataTable();
 
             using (var package = new ExcelPackage(new FileInfo(filePath)))
             {
-                var ws = package.Workbook.Worksheets[0]; //nic nie mówiące nazwy zmiennych; i tu jest takie miejsce gdzie var  zaciemnia, bo nie wiadomo wprost jakiego typu jest Worksheets[0]
-                if (ws.Dimension == null) return dt;
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                if (worksheet.Dimension == null) return dt;
 
-                int rows = ws.Dimension.Rows;
-                int cols = Math.Min(maxCols, ws.Dimension.Columns);
+                int rows = worksheet.Dimension.Rows;
+                int cols = Math.Min(maxCols, worksheet.Dimension.Columns);
 
-                for (int c = 1; c <= cols; c++) //to też dla czytelności mogło by być w osobnej funkcji List<DataColumns> GenerateColumns() i potem dt.Columns.AddRange(columns)
-                {
-                    string colName = ws.Cells[1, c].Text;
-                    dt.Columns.Add(string.IsNullOrWhiteSpace(colName) ? $"Column {c}" : colName);
-                }
-
+                GenerateColumns(worksheet, dt, cols);
 
                 for (int r = 2; r <= Math.Min(maxRows + 1, rows); r++)
                 {
-                    DataRow dr = dt.NewRow(); //metoda DataRow CreateRow()
-                    for (int c = 1; c <= cols; c++)
-                    {
-                        dr[c - 1] = ws.Cells[r, c].Text;
-                    }
+                    DataRow dr = CreateRow(worksheet, dt, r, cols);
                     dt.Rows.Add(dr);
                 }
             }
 
             return dt;
+        }
+
+        private void GenerateColumns(ExcelWorksheet worksheet, DataTable dt, int cols)
+        {
+            for (int c = 1; c <= cols; c++)
+            {
+                string colName = worksheet.Cells[1, c].Text;
+                dt.Columns.Add(string.IsNullOrWhiteSpace(colName) ? $"Column {c}" : colName);
+            }
+        }
+
+        private DataRow CreateRow(ExcelWorksheet worksheet, DataTable dt, int rowIndex, int cols)
+        {
+            DataRow dr = dt.NewRow();
+            for (int c = 1; c <= cols; c++)
+            {
+                dr[c - 1] = worksheet.Cells[rowIndex, c].Text;
+            }
+            return dr;
         }
     }
 }
